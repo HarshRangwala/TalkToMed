e2f7848402a3436036b5033b906e0c69b7b6d172
Stephen H <shapp@masonlive.gmu.edu>
2023-03-26 11:31:06 -0400

    Finishing stuff up

--- send-sms/sms-service.js
+++ send-sms/sms-service.js
@@ -1,12 +1,14 @@
 const sendSMS = require('./send-sms.js')
+const fs = require('fs')
+const concernForm = fs.readFileSync('./concern-form.html').toString();
 
 const express = require('express')
 const app = express()
-app.use(express.text({type: 'application.json'}))
-var port = process.env.PORT || 50000;
+app.use(express.text())
+var port = process.env.PORT || 3000;
 
 sendSMS.initialize();
-
+/*
 app.post('/', (req, res) => {
     return new Promise(() => {
 
@@ -40,8 +42,28 @@ app.post('/', (req, res) => {
 
         res.status(200).send("SMS sent");
     })
+}) */
+
+app.get('/concern-form', (req, res) => {
+    res.status(200).send(concernForm)
 })
 
+app.post('/concern-form', (req, res) => {
+    sendSMS.send('+15403310778', "TalkToMed: One of your patients have submitted a health concern. Log in to view: https://www.talktomed.com")
+    res.status(200).send("<head><style>body: {test-align: center;}</style></head><body><h3>Concern saved!</h3><h4>Your doctor has been notified</h4><a href='/concern-form'> Return</a></body>")
+})
+
+app.get('/file/:filename', (req, res) => {
+    return new Promise(() => {
+        const file = fs.readFileSync(req.params.filename)
+        if (file) {
+            res.status(200).send(file)
+        }
+        else {
+            res.status(404).send()
+        }
+    })
+})
 app.listen(port, () => console.log(`sms-service starting on port ${port}`));
 
 module.exports = app;
